806fff74b4ee0e2dc4b8ec488db6828243abeacd
Ussaid <imtiazussaid@gmail.com>
2023-11-12 20:18:17 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,170 @@
+/* eslint-disable @next/next/no-img-element */
+// @ts-nocheck
+
+export default function Home() {
+  return (
+    <div className="text-gray-800 bg-gray-200 p-10 m-10">
+      <div className="flex bg-white p-10 m-10 shadow-2xl justify-around flex-wrap">
+        <div className="text-center px-6 py-4">
+          <img
+            className="w-32 h-32 object-cover rounded-full object-top"
+            src="https://images.pexels.com/photos/7242908/pexels-photo-7242908.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=150"
+            alt="photo1"
+          />
+          <h3 className="font-bold text-xl">Alexa Kardi</h3>
+          <p className="text-sm text-gray-600">Founder and CEO</p>
+        </div>
+        <div className="text-center px-6 py-4">
+          <img
+            className="w-32 h-32 object-cover rounded-full object-top"
+            src=" https://images.pexels.com/photos/697509/pexels-photo-697509.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=150"
+            alt=""
+          />
+          <h3 className="font-bold text-xl">Tavell Monroe</h3>
+          <p className="text-sm text-gray-600">Web Developer</p>
+        </div>
+        <div className="text-center px-6 py-4">
+          <img
+            className="w-32 h-32 object-cover rounded-full object-top"
+            src="https://images.pexels.com/photos/3763188/pexels-photo-3763188.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=150n bbbbbbbb  "
+            alt="photo2"
+          />
+          <h3 className="font-bold text-xl">Adale Smith</h3>
+          <p className="text-sm text-gray-600">Marketing Specialist</p>
+        </div>
+        <div className="text-center px-6 py-4">
+          <img
+            className="w-32 h-32 object-cover rounded-full object-top"
+            src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=300"
+            alt="photo2"
+          />
+          <h3 className="font-bold text-xl">Thomas Mason</h3>
+          <p className="text-sm text-gray-600">UX Designer</p>
+        </div>
+      </div>
+    </div>
+
+    // <div class="flex justify-between">
+    //   <div class="w-[200px] m-3 p-6 bg-blue-100 rounded-lg">
+    //     <p class="font-serif italic text-lg text-gray-700 leading-snug mb-1">
+    //       “The success combination in business is: Do what you do better... and:
+    //       do more of what you do.”
+    //     </p>
+    //     <span class="text-sm text-gray-500">- David J. Schwartz</span>
+    //   </div>
+    //   <div class="w-[200px] m-3 p-6 bg-blue-100 rounded-lg">
+    //     <p class="font-serif italic text-lg text-gray-700 leading-snug mb-1">
+    //       “Give out what you most want to come back.”
+    //     </p>
+    //     <span class="text-sm text-gray-500">- Robin Sharma</span>
+    //   </div>
+    //   <div class="w-[200px] m-3 p-6 bg-blue-100 rounded-lg">
+    //     <p class="font-serif italic text-lg text-gray-700 leading-snug mb-1">
+    //       “You don't have to be great at something to start, but you have to
+    //       start to be great at something.”
+    //     </p>
+    //     <span class="text-sm text-gray-500">- Zig Ziglar</span>
+    //   </div>
+    // </div>
+
+    // <div className= "flex gap-6 flex-wrap ml-5">
+    //   <div className="bg-red-400 h-[200px] w-[200px]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non vitae qui corrupti consequatur facere libero.</div>
+    //   <div className="bg-red-400 h-[200px] w-[200px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse officiis ipsum earum expedita quod nostrum.</div>
+    //   <div className="bg-red-400 h-[200px] w-[200px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium tempore culpa nesciunt voluptas sapiente assumenda voluptate excepturi hic.</div>
+    // </div>
+
+    // <div className="flex justify-center items-center h-screen w-screen bg-gray-300">
+    //   <div className=" flex flex-col w-[200px] gap-4 bg-white p-4 shadow-2xl">
+    //     <div className= "font-extrabold text-2xl text-blue-700">
+    //       Some text
+    //     </div>
+    //     <div className="text-sm">
+    //       Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius labore unde recusandae quidem tempora eum, natus consequatur voluptatem hic nisi. Eius dignissimos quia blanditiis!
+    //     </div>
+    //     <div className="flex justify-between text-sm">
+    //       <button className="bg-blue-400 p-2 rounded-md">Prev</button>
+    //       <button className="bg-blue-400 p-2 rounded-md">Next</button>
+
+    //     </div>
+    //   </div>
+
+    //   </div>
+
+    //     <div>
+    //       <div className=" bg-gradient-to-l from-red-500 to-blue-900 ">
+
+    //        <div className=" bg-gray-200 mt-20">
+    //          <p>This is a first line</p>
+    //          <button className="bg-red-200 p-3 m-5">Button</button>
+    //          <p>This is a last line</p>
+    //        </div>
+
+    //  <div className="mt-10">
+    //    <button className=" border border-black p-7">one</button>
+
+    //    <button className=" border border-black mx-10">two</button>
+
+    //    <button className=" border border-black border-4 p-2">three</button>
+
+    //  </div>
+    //      <div className="my-10 mx-2">
+    //    <button className=" border border-black border-4 p-3 rounded-xl">four</button>
+    //    <button className=" border border-black border-4 p-3 rounded-full mx-4">five</button>
+
+    //      </div>
+
+    //      <div className="my-10 mx-2">
+    //    <button className=" border p-5 bg-white shadow-md">card 1</button>
+    //    <button className=" border  p-5 mx-4 bg-white shadow-lg">card 2</button>
+    //    <button className=" border p-5  mx-4 bg-white shadow-2xl">card 3</button>
+    //    <button className=" border  p-5 mx- bg-white shadow-inner">card 4</button>
+
+    //      </div>
+
+    //  <div className="m-10 flex space-x-10">
+    //        <button className="p-5 font-bold bg-red-300">card 1</button>
+    //        <button className="p-5 font-bold bg-green-300">card 2</button>
+    //        <button className="p-5 font-bold bg-gray-300">card 3</button>
+    //        <button className="p-5 font-bold bg-blue-300">card 4</button>
+    //      </div>
+
+    //      <div className="m-10 flex  flex-col space-y-10">
+    //        <button className="p-5 font-bold bg-red-300">card 1</button>
+    //        <button className="p-5 font-bold bg-green-300">card 2</button>
+    //        <button className="p-5 font-bold bg-gray-300">card 3</button>
+    //        <button className="p-5 font-bold bg-blue-300">card 4</button>
+    //      </div>
+    //     </div>
+
+    //  <div className="m-10 flex space-x-2 justify-center ">
+    //        <button className="p-5 font-bold bg-red-300">card 1</button>
+    //        <button className="p-5 font-bold bg-green-300">card 2</button>
+    //        <button className="p-5 font-bold bg-gray-300">card 3</button>
+    //        <button className="p-5 font-bold bg-blue-300">card 4</button>
+    //      </div>
+
+    //      <div className="flex space-x-2 justify-between">
+    //        <button className="p-5 font-bold bg-red-300">card 1</button>
+    //        <button className="p-5 font-bold bg-green-300">card 2</button>
+    //        <button className="p-5 font-bold bg-gray-300">card 3</button>
+    //        <button className="p-5 font-bold bg-blue-300">card 4</button>
+    //      </div>
+
+    //  <div className="flex space-x-2 justify-around my-4 p-10 flex-wrap">
+    //        <button className="p-5 font-bold bg-red-300">card 1</button>
+    //        <button className="p-5 font-bold bg-green-300">card 2</button>
+    //        <button className="p-5 font-bold bg-gray-300">card 3</button>
+    //        <button className="p-5 font-bold bg-blue-300">card 4</button>
+    //      </div>
+
+    //   <div className="flex space-x-2 justify-evenly">
+    //        <button className="p-5 font-bold bg-red-300">card 1</button>
+    //        <button className="p-5 font-bold bg-green-300">card 2</button>
+    //        <button className="p-5 font-bold bg-gray-300">card 3</button>
+    //        <button className="p-5 font-bold bg-blue-300">card 4</button>
+    //      </div>
+
+    //      <Image src={"/panaverse.png"} alt={""} width={80} height={80}/>
+    //      </div>
+  );
+}
